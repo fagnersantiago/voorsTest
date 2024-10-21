a5b216f2fb8d847ad16b30c6202f1bb36ac29b7c
Fagner Santiago <fagner.santiago@hotmail.com>
2024-10-21 11:38:10 -0300

    fix: removing appError import

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,32 +1,14 @@
+import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
-import "express-async-errors"; 
-import '../../container';       
+import '../../container';
+import { router } from '../routes';
 
-import { setupSwagger } from './swagger'; 
-import { router } from '../routes';   
-import { AppError } from "../../../shared/error/AppError";     
 
 const app = express();
-
 app.use(express.json());
-
-setupSwagger(app);
 
 app.use(router);
 
-app.use(
-    (err: Error, response: Response) => {
-      if (err instanceof AppError) {
-        return response.status(Number(err.statusCode)).json({
-          message: err.message,
-        });
-      }
-  
-      return response.status(500).json({
-        status: "error",
-        message: `internal error - ${err.message}`,
-      });
-    }
-  );
+
 
 export { app };
